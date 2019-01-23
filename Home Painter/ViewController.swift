91a7852513191ae2dc9cb48b734a7f9e38a4df90
Robert Wan <Run.Ting.Wan@ibm.com@roberts-mbp.blr.can.ibm.com>
2019-01-23 12:35:02 -0500

    first commit

---
+++ Home Painter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Painter
+//
+//  Created by Robert Wan on 2019-01-18.
+//  Copyright © 2019 App. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import SceneKit
+
+class ViewController: UIViewController, ARSessionObserver{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
