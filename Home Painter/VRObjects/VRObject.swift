234d552150d910fe249a77361e3b2cb551625be0
Robert Wan <Run.Ting.Wan@ibm.com@roberts-mbp.blr.can.ibm.com>
2019-01-24 17:39:10 -0500

    virtual object extension

--- Home Painter/VRObjects/VRObject.swift
+++ Home Painter/VRObjects/VRObject.swift
@@ -37,7 +37,7 @@ class virtualObject: SCNReferenceNode {
     private var currentAlignment: ARPlaneAnchor.Alignment = .horizontal
     
     // whether the obj is currently changing alignment (????
-    private var changingAlignment: Bool = false
+    private var isChangingAlignment: Bool = false
     
     
     
@@ -99,5 +99,132 @@ class virtualObject: SCNReferenceNode {
             positionOffsetFromCamera = simd_normalize(positionOffsetFromCamera)
             positionOffsetFromCamera *= 10
         }
+        
+        // smoothmovement: compute the average distance between the camera and the object for the last 10 update
+        // The distance is calculated from the camera to the object. Averaging the distance does _not_ make the object lag
+        if smoothMovement {
+            let hitResultDistance = simd_length(positionOffsetFromCamera)
+            // add the lastest position and keep 10 distances to smooth the movement
+            recentVRObjectDistance.append(hitResultDistance)
+            recentVRObjectDistance = Array(recentVRObjectDistance.suffix(10))
+            let averageDistance = recentVRObjectDistance.average!
+            let averageDistancePosition = simd_normalize(positionOffsetFromCamera) * averageDistance
+            simdPosition = positionOffsetFromCamera + averageDistancePosition
+        } else {
+            simdPosition = cameraWorldPosition + positionOffsetFromCamera
+        }
+        
+        updateAlignment(to: alignment, transform: newTransform, allowAnimation: allowAnimation)
+    }
+    
+    // setting object's alignment
+    func updateAlignment(to newAlignment: ARPlaneAnchor.Alignment, transform: float4x4, allowAnimation: Bool){
+        if isChangingAlignment {
+            return
+        }
+        // only animate if alignment is changed
+        let animationDuration = (newAlignment != currentAlignment && allowAnimation) ? 0.5 : 0
+        var newObjectRotation: Float?
+        switch (currentAlignment, newAlignment){
+        // doesnt need to be changed, unlike vertical
+        case (.horizontal, .horizontal):
+            return
+        // when changing to horizontal rotation, restore to previous horizontal rotation
+        case (.horizontal, .vertical):
+            newObjectRotation = rotationWhenAlignedHorizontally
+        //when changing to vertical rotation, reset the object's rotation (y-up)
+        case (.vertical, .horizontal):
+            newObjectRotation = 0.0001
+        default:
+            break
+        }
+        currentAlignment = newAlignment
+        
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = animationDuration
+        SCNTransaction.completionBlock = {
+            self.isChangingAlignment = false
+        }
+        isChangingAlignment = true
+        
+        // use filtered position rather than exact position
+        var multableTransform = transform
+        multableTransform.translation = simdWorldPosition
+        simdTransform = multableTransform
+        
+        if newObjectRotation != nil {
+            newObjectRotation = newObjectRotation!
+        }
+        
+        SCNTransaction.commit()
+    }
+    
+    // adjust on to plane anchor
+    func adjustOnToPlaneAnchor(_ anchor:ARPlaneAnchor, using node: SCNNode){
+        // test if the alignment of the plane is compatible with the object's allowed placement
+        if !allowedAlignments.contains(anchor.alignment){
+            return
+        }
+        // get the object's position in the plane's coord system
+        // position is self.position, a member of SCNReferenceNode, inherited from SCNNode
+        let planePosition = node.convertPosition(position, to: parent)
+        
+        // check if obj is already in the plane
+        guard planePosition.y != 0 else {return}
+        
+        // add 10% tolerance at the corner of the plane
+        let tolerance:Float = 0.1
+        
+        let minX = anchor.center.x - anchor.extent.x / 2 - anchor.extent.x * tolerance
+        let maxX = anchor.center.x + anchor.extent.x / 2 + anchor.extent.x * tolerance
+        let minZ = anchor.center.z - anchor.extent.z / 2 - anchor.extent.z * tolerance
+        let maxZ = anchor.center.z + anchor.extent.z / 2 + anchor.extent.z * tolerance
+        
+        // return if x and z are not in range
+        guard (minX...maxX).contains(planePosition.x) && (minZ...maxZ).contains(planePosition.z) else {
+            return
+        }
+        
+        // move on to the plane if it is close, but dont update if it is too close
+        let verticalAllowance: Float = 0.05
+        let epsilon: Float = 0.001
+        let distanceToPlane = abs(planePosition.y)
+        if distanceToPlane > epsilon && distanceToPlane < verticalAllowance {
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = CFTimeInterval(distanceToPlane * 500) // 2mm/s
+            SCNTransaction.animationTimingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+            position.y = anchor.transform.columns.3.y
+            updateAlignment(to: anchor.alignment, transform: simdWorldTransform, allowAnimation: false)
+            SCNTransaction.commit()
+        }
+    }
+}
+
+// static properties and methods
+extension virtualObject {
+    // Load all models from models.scnassets
+    static let availableObjects: [virtualObject] = {
+        let modelsURL = Bundle.main.url(forResource: "Model.scnassets", withExtension: nil)!
+        let fileEnumerator = FileManager().enumerator(at: modelsURL, includingPropertiesForKeys: [])!
+        
+        return fileEnumerator.compactMap {element in
+            let url = element as! URL
+            guard url.pathExtension == "scn" && !url.path.contains("lightning") else {return nil}
+            return virtualObject(url: url)
+        }
+    }()
+}
+
+extension Collection where Element == Float, Index == Int {
+    // return the mean of a array of float numbers
+    var average: Float? {
+        guard !isEmpty else {
+            return nil
+        }
+        let sum = reduce(Float(0)) {
+            current, next -> Float in
+            return current + next
+        }
+        return sum/Float(count)
     }
 }
