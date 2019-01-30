f6e80b6156904ae4a54573e978e552acab7f06a4
Robert Wan <Run.Ting.Wan@ibm.com@roberts-mbp.blr.can.ibm.com>
2019-01-30 16:35:26 -0500

    start pan gesture function

---
+++ Home Painter/ThresholdPanGesture.swift
@@ -0,0 +1,10 @@
+//
+//  ThresholdPanGesture.swift
+//  Home Painter
+//
+//  Created by Robert Wan on 2019-01-30.
+//  Copyright © 2019 App. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIGestureRecognizerSubclass
